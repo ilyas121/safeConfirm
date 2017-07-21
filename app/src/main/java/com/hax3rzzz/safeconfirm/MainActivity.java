ed974735a41ef2754fc31e2282112bd31a11405c
ilyas121 <ilyassalhi@gmail.com>
2017-07-21 18:40:59 -0400

    sms txting (lowkey spam)

--- app/src/main/java/com/hax3rzzz/safeconfirm/MainActivity.java
+++ app/src/main/java/com/hax3rzzz/safeconfirm/MainActivity.java
@@ -2,6 +2,8 @@ package com.hax3rzzz.safeconfirm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("7813011976", null, "SPAM", null, null);
     }
 }
