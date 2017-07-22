c8705d5ff190ddc361ce07b807113bdf7f92bc4d
ilyas121 <ilyassalhi@gmail.com>
2017-07-21 23:22:53 -0400

    Gyro added

--- app/src/main/java/com/hax3rzzz/safeconfirm/MainActivity.java
+++ app/src/main/java/com/hax3rzzz/safeconfirm/MainActivity.java
@@ -26,13 +26,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        //SMS TEXTING
+    public void sendTXT(){
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("7813011976", null, "UR FRIEND DON GOOFED", null, null);
+    }
 
+    public long map(long x, long in_min, long in_max, long out_min, long out_max)
+    {
+        if(x < in_max) {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+        else{
+            return 10000000;
+        }
     }
 
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
+
+        float weightAC = 0;
+        float weightGY = 0;
+        float weightALT = 0;
 
         if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
             float x = sensorEvent.values[0];
@@ -50,16 +66,45 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                 float jerk = (float)Math.sqrt(jerkX*jerkX + jerkY*jerkY + jerkZ*jerkZ);
 
-                if (jerk > 5) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage("7813011976", null, "U GOOD FAM??", null, null);
-                }
 
                 last_x = x;
                 last_y = y;
                 last_z = z;
+
+                weightAC = map((long) jerk, 0, 10, 0, 10);
             }
         }
+
+        if (mySensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+                float omegaX = x - last_x;
+                float omegaY = y - last_y;
+                float omegaZ = z - last_z;
+
+                float omega = (float)Math.sqrt(omegaX*omegaX + omegaY*omegaY + omegaZ*omegaZ);
+
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+
+                weightGY = map((long)omega, 0, (long)(4 * Math.PI), 0, 10);
+            }
+        }
+
+
+        if(weightALT + weightAC +weightGY > 10){
+            sendTXT();
+        }
+
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
