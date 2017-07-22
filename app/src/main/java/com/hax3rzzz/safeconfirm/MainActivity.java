b42c17858fe5a58dd49e5de717b49b41bfb6d7a2
ilyas121 <ilyassalhi@gmail.com>
2017-07-21 20:43:19 -0400

    Accelerometer good

--- app/src/main/java/com/hax3rzzz/safeconfirm/MainActivity.java
+++ app/src/main/java/com/hax3rzzz/safeconfirm/MainActivity.java
@@ -4,14 +4,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.telephony.SmsManager;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.content.Context;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage("7813011976", null, "SPAM", null, null);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+        //SMS TEXTING
+
     }
+
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+                float jerkX = x - last_x;
+                float jerkY = y - last_y;
+                float jerkZ = z - last_z;
+
+                float jerk = (float)Math.sqrt(jerkX*jerkX + jerkY*jerkY + jerkZ*jerkZ);
+
+                if (jerk > 5) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("7813011976", null, "U GOOD FAM??", null, null);
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
 }
